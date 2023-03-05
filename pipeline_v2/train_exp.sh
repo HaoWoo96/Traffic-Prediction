5e3767994faa10cfe32f4047d322dfafce4200dd
Hao Wu <haowu3@andrew.cmu.edu>
2023-03-05 13:11:36 -0500

    update train_exp

--- pipeline_v2/train_exp.sh
+++ pipeline_v2/train_exp.sh
@@ -1,47 +1,51 @@
 #!/bin/bash
 
-# python train.py --task LR --use_density --use_speed --use_truck_spd --use_pv_spd --use_expectation
-# python train.py --task rec --use_density --use_speed --use_truck_spd --use_pv_spd --use_expectation
-# python train.py --task nonrec --use_density --use_speed --use_truck_spd --use_pv_spd --use_expectation
-# python train.py --task finetune --use_density --use_speed --use_truck_spd --use_pv_spd --use_expectation
+# python train.py --task base --use_dens --use_spd_all --use_spd_truck --use_spd_pv
+# python train.py --task base --use_spd_all --use_spd_truck --use_spd_pv
+# python train.py --task base --use_dens --use_spd_all 
+# python train.py --task base
 
 
-# python train.py --task LR --use_speed --use_truck_spd --use_pv_spd --use_expectation
-# python train.py --task rec  --use_speed --use_truck_spd --use_pv_spd --use_expectation
-# python train.py --task nonrec --use_speed --use_truck_spd --use_pv_spd --use_expectation
-# python train.py --task finetune --use_speed --use_truck_spd --use_pv_spd --use_expectation
 
-# python train.py --task LR --use_density --use_speed --use_expectation
-# python train.py --task rec --use_density --use_speed --use_expectation
-# python train.py --task nonrec --use_density --use_speed --use_expectation
-# python train.py --task finetune --use_density --use_speed --use_expectation
+# python train.py --task LR --use_dens --use_spd_all --use_spd_truck --use_spd_pv --use_expectation
+# python train.py --task rec --use_dens --use_spd_all --use_spd_truck --use_spd_pv --use_expectation
+# python train.py --task nonrec --use_dens --use_spd_all --use_spd_truck --use_spd_pv --use_expectation
+# python train.py --task finetune --use_dens --use_spd_all --use_spd_truck --use_spd_pv --use_expectation
+
+
+# python train.py --task LR --use_spd_all --use_spd_truck --use_spd_pv --use_expectation
+# python train.py --task rec  --use_spd_all --use_spd_truck --use_spd_pv --use_expectation
+# python train.py --task nonrec --use_spd_all --use_spd_truck --use_spd_pv --use_expectation
+# python train.py --task finetune --use_spd_all --use_spd_truck --use_spd_pv --use_expectation
+
+# python train.py --task LR --use_dens --use_spd_all --use_expectation
+# python train.py --task rec --use_dens --use_spd_all --use_expectation
+# python train.py --task nonrec --use_dens --use_spd_all --use_expectation
+# python train.py --task finetune --use_dens --use_spd_all --use_expectation
 
 # python train.py --task LR --use_expectation
 # python train.py --task rec --use_expectation
 # python train.py --task nonrec --use_expectation
 # python train.py --task finetune --use_expectation
 
-# python train.py --task base --use_density --use_speed --use_truck_spd --use_pv_spd
-python train.py --task base --use_speed --use_truck_spd --use_pv_spd
-python train.py --task base --use_density --use_speed 
-python train.py --task base
 
-python train.py --task LR --use_density --use_speed --use_truck_spd --use_pv_spd 
-python train.py --task rec --use_density --use_speed --use_truck_spd --use_pv_spd 
-python train.py --task nonrec --use_density --use_speed --use_truck_spd --use_pv_spd 
-python train.py --task finetune --use_ density --use_speed --use_truck_spd --use_pv_spd 
 
-python train.py --task LR --use_speed --use_truck_spd --use_pv_spd 
-python train.py --task rec  --use_speed --use_truck_spd --use_pv_spd 
-python train.py --task nonrec --use_speed --use_truck_spd --use_pv_spd 
-python train.py --task finetune --use_speed --use_truck_spd --use_pv_spd 
+# python train.py --task LR --use_dens --use_spd_all --use_spd_truck --use_spd_pv 
+# python train.py --task rec --use_dens --use_spd_all --use_spd_truck --use_spd_pv 
+# python train.py --task nonrec --use_dens --use_spd_all --use_spd_truck --use_spd_pv 
+python train.py --task finetune --use_dens --use_spd_all --use_spd_truck --use_spd_pv 
 
-python train.py --task LR --use_density --use_speed 
-python train.py --task rec --use_density --use_speed 
-python train.py --task nonrec --use_density --use_speed 
-python train.py --task finetune --use_density --use_speed
+# python train.py --task LR --use_spd_all --use_spd_truck --use_spd_pv 
+# python train.py --task rec  --use_spd_all --use_spd_truck --use_spd_pv 
+# python train.py --task nonrec --use_spd_all --use_spd_truck --use_spd_pv 
+# python train.py --task finetune --use_spd_all --use_spd_truck --use_spd_pv 
 
-python train.py --task LR 
-python train.py --task rec 
-python train.py --task nonrec 
-python train.py --task finetune 
+# python train.py --task LR --use_dens --use_spd_all 
+# python train.py --task rec --use_dens --use_spd_all 
+# python train.py --task nonrec --use_dens --use_spd_all 
+# python train.py --task finetune --use_dens --use_spd_all
+
+# python train.py --task LR 
+# python train.py --task rec 
+# python train.py --task nonrec 
+# python train.py --task finetune 
